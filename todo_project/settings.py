aa5505d670a16d6ea9dbacb986827f90a1419d19
Anubhavsaxena2004 <anubhavsaxenavibhu@gmail.com>
2026-05-03 23:25:10 +0530

    Add 'random' import to settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path , maths
+from pathlib import Path , maths, random
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
